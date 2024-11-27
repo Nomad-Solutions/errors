64735d23ed293f72f6c7129b518b5badeee244a5
magnus-bb <magnus.borregaard@gmail.com>
2024-11-27 23:06:38 +0100

    feat: add elysia error handling method

---
+++ lib/elysia.ts
@@ -0,0 +1,21 @@
+import { isHttpError } from './assertions';
+import { HttpResponse } from './http-responses';
+import { formatErrorMessage, logTraceableError } from './utils';
+
+// This can be passed directly to Elysia.onError
+export function onError({ error, set }: {
+	error: unknown,
+	set: { status: number }
+}) {
+	logTraceableError(error);
+
+	if (isHttpError(error)) {
+		set.status = error.statusCode;
+
+		return formatErrorMessage(error.name, error.message) || error.httpMessage;
+	}
+
+	// All non-custom errors are treated as 500 Internal Server Error and message is obscured
+	set.status = HttpResponse.InternalServerError.code;
+	return formatErrorMessage(HttpResponse.InternalServerError.name, HttpResponse.InternalServerError.description);
+}
